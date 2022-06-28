e9a967953ba7e4feb242a6cabade653c7e09a747
kyian-web <example@gmail.com>
2022-06-28 22:37:44 +0300

    Pong Class

--- game.js
+++ game.js
@@ -1,3 +1,63 @@
+class Ping {
+    constructor() {
+        this.canvas = document.querySelector('#pong');
+        this.context = this.canvas.getContext('2d');
+        this.ball = {
+            x: this.canvas.width / 2,
+            y: this.canvas.height / 2,
+            radius: 10,
+            speed: 20,
+            velocityX: 5,
+            velocityY: 5,
+            color: '#fff'
+        }
+    }
+
+    drawRect(x, y, width, height, color) {
+        this.context.beginPath();
+        this.context.fillStyle = color;
+        this.context.fillRect(x, y, width, height);
+        this.context.closePath();
+    }
+
+    drawCircle(x, y, radius, color) {
+        this.context.beginPath();
+        this.context.fillStyle = color;
+        this.context.arc(x, y, radius, 0, Math.PI*2, false);
+        this.context.closePath();
+        this.context.fill();
+    }
+
+    renderGame() {
+        this.drawRect(0, 0, this.canvas.width, this.canvas.height, '#0660a0');
+        this.drawCircle( this.ball.x, this.ball.y, this.ball.radius, this.ball.color);
+    }
+
+    update() {
+        this.ball.x += this.ball.velocityX;
+        this.ball.y += this.ball.velocityY;
+    
+        if (this.ball.y + this.ball.radius > this.canvas.height || this.ball.y - this.ball.radius < 0) {
+            this.ball.velocityY = - this.ball.velocityY;
+        } else if (this.ball.x + this.ball.radius > this.canvas.width || this.ball.x - this.ball.radius < 0) {
+            this.ball.velocityX = -this.ball.velocityX;
+        }
+    }
+
+    render() {
+        setInterval(() => {
+            this.renderGame();
+            this.update();
+        }, this.ball.speed)
+    }
+}
+
+new Ping().render();
+
+
+
+// ###########################################################
+
 // const cvs = document.querySelector('#pong');
 // const ctx = cvs.getContext('2d');
 
@@ -51,65 +111,65 @@
 
 // ###################################
 
-const cvs = document.querySelector('#pong');
-const ctx = cvs.getContext('2d');
+// const cvs = document.querySelector('#pong');
+// const ctx = cvs.getContext('2d');
 
-// let img = new Image();
-// img.src = 'ball.png';
+// // let img = new Image();
+// // img.src = 'ball.png';
 
-const iconBall = {
-    src: 'ball.png',
-    x: cvs.width/2,
-    y: cvs.height/2,
-    width: 20,
-    height: 20,
-    speed: 20,
-    velocityX: 5,
-    velocityY: 5
-}
+// const iconBall = {
+//     src: 'ball.png',
+//     x: cvs.width/2,
+//     y: cvs.height/2,
+//     width: 20,
+//     height: 20,
+//     speed: 20,
+//     velocityX: 5,
+//     velocityY: 5
+// }
 
-function createBall(srcBall, x, y, width, height) {
-    let img = new Image();
-    img.src = srcBall;
-    ctx.drawImage(img, x, y, width, height);
-}
+// function createBall(srcBall, x, y, width, height) {
+//     let img = new Image();
+//     img.src = srcBall;
+//     ctx.drawImage(img, x, y, width, height);
+// }
 
-const ball = {
-    x: cvs.width / 2,
-    y: cvs.height / 2,
-    radius: 10,
-    speed: 20,
-    velocityX: 5,
-    velocityY: 5,
-    color: '#fff'
-}
+// const ball = {
+//     x: cvs.width / 2,
+//     y: cvs.height / 2,
+//     radius: 10,
+//     speed: 20,
+//     velocityX: 5,
+//     velocityY: 5,
+//     color: '#fff'
+// }
 
-function drawRect(x, y, width, height, color) {
-    ctx.beginPath();
-    ctx.fillStyle = color;
-    ctx.fillRect(x, y, width, height);
-    ctx.closePath();
-}
+// function drawRect(x, y, width, height, color) {
+//     ctx.beginPath();
+//     ctx.fillStyle = color;
+//     ctx.fillRect(x, y, width, height);
+//     ctx.closePath();
+// }
 
-function render() {
-    drawRect(0, 0, cvs.width, cvs.height, '#0660a0');
-    createBall(iconBall.src, iconBall.x, iconBall.y, iconBall.width, iconBall.height);
-}
+// function render() {
+//     drawRect(0, 0, cvs.width, cvs.height, '#0660a0');
+//     createBall(iconBall.src, iconBall.x, iconBall.y, iconBall.width, iconBall.height);
+// }
 
-function update() {
-    iconBall.x += iconBall.velocityX;
-    iconBall.y += iconBall.velocityY;
+// function update() {
+//     iconBall.x += iconBall.velocityX;
+//     iconBall.y += iconBall.velocityY;
 
-    if (iconBall.y + iconBall.width > cvs.height || iconBall.y - iconBall.width < 0) {
-        iconBall.velocityY = -iconBall.velocityY;
-    } else if (iconBall.x + iconBall.width > cvs.width || iconBall.x - iconBall.width < 0) {
-        iconBall.velocityX = -iconBall.velocityX;
-    }
-}
+//     if (iconBall.y + iconBall.width > cvs.height || iconBall.y - iconBall.width < 0) {
+//         iconBall.velocityY = -iconBall.velocityY;
+//     } else if (iconBall.x + iconBall.width > cvs.width || iconBall.x - iconBall.width < 0) {
+//         iconBall.velocityX = -iconBall.velocityX;
+//     }
+// }
 
-function game() {
-    render();
-    update();
-}
+// function game() {
+//     render();
+//     update();
+// }
 
-setInterval(game, iconBall.speed);
+// setInterval(game, iconBall.speed);
